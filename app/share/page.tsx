54cc618a6c7591ad2011855c146f9bd5fe3924a6
kenshin <kenshin.va@gmail.com>
2025-08-10 17:19:08 +1200

    fix

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,8 +1,12 @@
 // app/share/page.tsx (Server page, decrypt on server)
 import ClientShare from '@/components/ClientShare'; // Client component for handling user interaction
 
-export default async function Share({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
-  const data64 = searchParams.data || '';
+export default function Share({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+  const data64 = typeof searchParams.data === 'string' 
+    ? searchParams.data 
+    : Array.isArray(searchParams.data) 
+      ? searchParams.data[0] || '' 
+      : '';
 
   return <ClientShare data64={data64}/>;
 }
